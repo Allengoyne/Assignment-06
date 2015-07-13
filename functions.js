54b6db744459ce5bdd7d1a272fb5c4d94091ad81
Allen Goyne <allen.goyne@gmail.com>
2015-07-13 18:41:44 -0500

    homework done...for now

--- functions.js
+++ functions.js
@@ -23,10 +23,10 @@
 // Q1
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
-function sum(){
-  var num1 = "a";
-  var num2 = "b";
-   return a + b;
+function sum(a, b){
+  var num1 = Number(a);
+  var num2 = Number(b);
+   return num1 + num2;
 }
 
 
@@ -37,12 +37,13 @@ function sum(){
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
 
-function avg(){
-  var num1 = "a";
-  var num2 = "b";
-  var num3 = "c";
+function avg(a, b, c){
+  var num1 = Number(a);
+  var num2 = Number(b);
+  var num3 = Number(c);
   var sum = (num1 + num2 + num3);
-  return(sum/3);
+
+    return(sum / 3);
 }
 
 
@@ -51,6 +52,9 @@ function avg(){
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
 
+function getLength(str){
+   return str.length;
+}
 
 
 
@@ -59,6 +63,9 @@ function avg(){
 // two parameters and returns `true` if the second
 // parameter is greater than the first. Otherwise
 // the function should return `false`.
+function greaterThan(param1, param2){
+  return param1 > param2;
+}
 
 
 
@@ -68,6 +75,9 @@ function avg(){
 // single parameter and returns a string that
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
+function greet(name){
+  console.log("Hello, " + name);
+}
 
 
 
@@ -82,7 +92,10 @@ function avg(){
 // strings. For example:
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
-
+function madlib(a, b, c, d){
+  return (a + "" + b + "" + c + "" + d + "" + "over the fence" );
+}
+console.log(madlib(a, b, c, d));
 
 
 
